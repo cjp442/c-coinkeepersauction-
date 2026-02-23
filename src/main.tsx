9283df543ab8b36f6974e94994c4bee7b6ec788d
cjp442 <cjptmp17@gmail.com>
2026-02-23 11:55:49 -0500

    Create React entry point component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Keepers Auction!</h1>
+        </div>
+    );
+};
+
+export default App;
