249199d9fe01df5dae6b04ebf61646c87f09d934
cjp442 <cjptmp17@gmail.com>
2026-02-23 10:12:26 -0500

    Created src/App.tsx with React Router v6 setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import GamePage from './GamePage';
-import AdminPage from './AdminPage';
-import ProfilePage from './ProfilePage';
-import Shop from './Shop';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Auction from './components/Auction';
+import Dashboard from './components/Dashboard';
+import Multiplayer from './components/Multiplayer';
+import Layout from './components/Layout';
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/game" component={GamePage} />
-        <Route path="/admin" component={AdminPage} />
-        <Route path="/profile" component={ProfilePage} />
-        <Route path="/shop" component={Shop} />
-      </Switch>
-    </Router>
-  );
-};
+function App() {
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Dashboard />} />
+                    <Route path='/auction' element={<Auction />} />
+                    <Route path='/multiplayer' element={<Multiplayer />} />
+                </Routes>
+            </Layout>
+        </Router>
+    );
+}
 
 export default App;
